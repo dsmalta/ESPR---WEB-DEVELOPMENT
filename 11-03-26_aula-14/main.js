7c50b77a91562024e95cb515d107342dfb8121b2
Laboratório FIAP <labsfiap@fiap.com.br>
2026-03-11 22:25:03 -0300

    aula 14 adicionada

---
+++ 11-03-26_aula-14/main.js
@@ -0,0 +1,25 @@
+
+// const BD = [
+//     {id: 1, nome: 'Produto 1', preco: 1500},
+//     {id: 2, nome: 'Produto 2', preco: 2500},
+//     {id: 3, nome: 'Produto 3', preco: 3500}
+// ]
+
+// const pedirProdutos = () => {
+//     return new Promise((resolve, reject) => {
+//         setTimeout(() => {
+//             resolve(BD)
+//         }, 5000)
+//     })
+// }
+// let produtos = []
+
+// const renderProdutos = (arr) => {
+//     console.log(produtos)
+// }
+
+// pedirProdutos()
+//     .then((res) => {
+//         produtos = res
+//         renderProdutos(produtos)
+//     })
