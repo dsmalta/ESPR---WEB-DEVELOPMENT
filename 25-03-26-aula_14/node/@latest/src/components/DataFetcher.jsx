656207f44f9f47213b7e276001ee608ecf1b7395
Laboratório FIAP <labsfiap@fiap.com.br>
2026-03-25 22:47:31 -0300

    aula 14 atualizada

---
+++ 25-03-26-aula_14/node/@latest/src/components/DataFetcher.jsx
@@ -0,0 +1,31 @@
+import React,{useState, useEffect} from "react";
+
+function DataFetcher() {
+    const [data, setData] = useState(null);
+    const [loading, setLoading] = useState(true);
+    const [error, setError] = useState(null);
+
+    useEffect(() => {
+        async function fetchData() {
+            try {
+                setLoading(true);
+                setError(null);
+
+                const response = await fetch('https://jsonplaceholder.typicode.com/posts/1');
+                
+                if(!response.ok) {
+                    throw new Error(`Erro de rede: ${response.status} ${response.statusText}`);
+                }
+
+                const jsonData = await response.json();
+
+                setData(jsonData);
+            }catch(err) {
+                console.error("Falha ao buscar dados: ", err);
+                setError(err.message);
+            }finally{
+                setLoading(false);
+            }
+        }
+    })
+}
